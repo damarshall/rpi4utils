e05f8355f7600dbb85e6ea65293369a815a4c720
David Marshall <davidamarshall@gmail.com>
2019-09-02 10:37:24 -0400

    clean up downloaded and built software, ready for clean build

---
+++ ubuntu-4-pi/clean.sh
@@ -0,0 +1,7 @@
+rm -rf ./firmware-nonfree
+rm -rf ./firmware-master
+rm -rf ./rpi-tools
+rm -rf ./toolchains
+rm -rf ./rpi-linux
+rm master.zip
+rm ubuntu-18.04.3-preinstalled-servier-arm64+raspi3.img.xz
